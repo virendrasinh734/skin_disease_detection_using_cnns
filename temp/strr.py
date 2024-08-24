6d97f2c6d9de6b8a5144c93b2a3f1aac3a71bc1e
Virendrasinh Khuman <virendrasinhkhuman@gmail.com>
2024-08-24 19:38:09 +0530

    restructuring and adding segmentation code

--- temp/strr.py
+++ temp/strr.py
@@ -9,20 +9,14 @@ model = tf.keras.models.load_model('DenseNetApproach.h5')
 # Streamlit app header
 st.title("Skin Disease Prediction")
 
-# File upload widget
 uploaded_file = st.file_uploader("Upload a skin condition image...", type=["jpg", "png", "jpeg"])
 
-# Check if an image has been uploaded
 if uploaded_file is not None:
-    # Display the uploaded image
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    # Preprocess the image (resize, normalize, etc.)
     processed_image = preprocess_input(image)
 
-    # Make predictions using your skin disease model
     prediction = your_skin_disease_model.predict(processed_image)
 
-    # Display the predicted disease name
     st.write(f"Predicted Disease: {prediction}")
